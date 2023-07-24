05054f17b1c203b4fa3a79103ce0cd1ab633bb9c
kameoka <e2058232@soka-u.jp>
2023-07-24 22:27:33 +0900

    read_busroute.js datashaping関数の追加

--- js/read_buspole.js
+++ js/read_buspole.js
@@ -30,7 +30,7 @@ const buspole_datashaping = async (list_buspole_data, use_list) => {
     };
 
     // 創価大学周辺のバス停か否かを判定
-    if (use_list.true_list.includes(buspole["dc:title"])) {
+    if (use_list.start_list.includes(buspole["dc:title"])) {
       // データの結合
       const plus_poledata = { busstopPoleNumber: buspole["odpt:busstopPoleNumber"], busroutePattern: buspole["odpt:busroutePattern"] };
       buspole_start[buspole["owl:sameAs"]] = Object.assign(dict_buspole, plus_poledata);
@@ -62,17 +62,17 @@ const buspole_datashaping = async (list_buspole_data, use_list) => {
   });
 
   // バス停基準データとバス停に発着するバスの系統リストの結合
-  const dict_buspole_data = { route_true: { buspole_data: buspole_start, busroute_data: list_busroute }, route_false: { buspole_data: buspole_goal_push } };
+  const dict_buspole_data = { route_list: list_busroute, route_start: buspole_start, route_goal: buspole_goal_push };
   return dict_buspole_data;
 };
 
 const main_buspole = async () => {
   // バス停データ一覧
   const NishiTokyoBus = "odpt.Operator:NishiTokyoBus";
-  const buspole_sodai_list = ["創価大東京富士美術館（正門）", "創価大学創大門", "創価大学栄光門"];
-  const buspole_hachi_list = ["八王子駅北口", "京王八王子駅"];
-  const buspole_all_list = buspole_sodai_list.concat(buspole_hachi_list);
-  const buspole_all_dict = { true_list: buspole_sodai_list, false_list: buspole_hachi_list };
+  const buspole_start_list = ["創価大東京富士美術館（正門）", "創価大学創大門", "創価大学栄光門"];
+  const buspole_goal_list = ["八王子駅北口", "京王八王子駅"];
+  const buspole_all_list = buspole_start_list.concat(buspole_goal_list);
+  const buspole_all_dict = { start_list: buspole_start_list, goal_list: buspole_goal_list };
 
   // バス停情報の取得
   let buspole_data = await read_buspole(buspole_all_list, NishiTokyoBus);
