2a856354bce1f010be3427e15f0849237937b5fe
kameoka <e2058232@soka-u.jp>
2023-07-25 23:32:03 +0900

    read_busroute.js 修正前

--- js/read_busroute.js
+++ js/read_busroute.js
@@ -11,15 +11,68 @@ const read_busroute = async (buspole) => {
   return busroute_data;
 };
 
-const busroute_datashaping = async (list_busroute_data) => {
+const busroute_datashaping = async (list_busroute_data, buspole) => {
+  const busstart_pole_list = buspole.route_start;
+  const busgoal_pole_list = buspole.route_goal;
+  // console.log(busstart_pole_list);
+  // console.log(busgoal_pole_list);
+  // const dict_busall_pole = Object.assign(buspole.route_start, buspole.route_goal);
+  let list_busroute = [];
+
+  // console.log(list_busroute_data);
+  // console.log(buspole);
+  // console.log(busstart_pole_list);
+  // console.log(busgoal_pole_list);
+
   list_busroute_data.forEach(async (busroute) => {
-    console.log(busroute["odpt:busstopPoleOrder"]);
+    let busstopPoleObject = {};
+    let busstopPoleList = [];
+
+    // バス停の固有識別子と順番の対応表を作成
+    busroute["odpt:busstopPoleOrder"].forEach(async (busstopPoleOrder) => {
+      busstopPoleObject[busstopPoleOrder["odpt:busstopPole"]] = { index: busstopPoleOrder["odpt:index"] };
+    });
+
+    // バス停と路線の情報から、路線情報にバス停情報を追加
+    const dict_bus_pole = { start_pole: busstart_pole_list[busroute["owl:sameAs"]], goal_pole: busgoal_pole_list[busroute["owl:sameAs"]] };
+    Object.keys(dict_bus_pole).forEach(async (buspole_key) => {
+      dict_bus_pole[buspole_key].forEach(async (buspole) => {
+        busstopPoleObject[buspole.busstopPoleSome][buspole_key] = buspole;
+      });
+    });
+    // console.log(busroute);
+    // console.log(busstopPoleObject);
+    // console.log(dict_busall_pole);
+    // busall_pole_list.forEach(async (buspole) => {
+    //   busstopPoleList.push({ index: busstopPoleObject[buspole]["index"], pole: buspole });
+    //   // busstopPoleObject[buspole.busstopPoleSome]["pole"] = buspole;
+    // });
+
+    // console.log(busstopPoleObject);
+    Object.keys(busstopPoleObject).forEach(async (busstopPole_key) => {
+      if ("start_pole" in busstopPoleObject[busstopPole_key]) {
+        busstopPoleList.push({ index: busstopPoleObject[busstopPole_key]["index"], pole: busstopPoleObject[busstopPole_key]["start_pole"] });
+      } else if ("goal_pole" in busstopPoleObject[busstopPole_key]) {
+        busstopPoleList.push({ index: busstopPoleObject[busstopPole_key]["index"], pole: busstopPoleObject[busstopPole_key]["goal_pole"] });
+      }
+    });
+
+    // バス停の情報のうち、必要なものを抽出
+    let dict_busroute = {
+      owlSameAs: busroute["owl:sameAs"],
+      title: busroute["dc:title"],
+      busstopPoleOrder: busstopPoleList,
+    };
+
+    // バス停一覧のリストに追加
+    list_busroute.push(dict_busroute);
   });
+  console.log(list_busroute);
 };
 
 const main_busroute = async (buspole) => {
   const busroute_data = await read_busroute(buspole);
-  busroute_datashaping(busroute_data);
+  busroute_datashaping(busroute_data, buspole);
   //   console.log(buspole);
   return 0;
 };
